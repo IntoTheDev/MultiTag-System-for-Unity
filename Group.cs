9c496192a6da44e5121bfef2f48693c79b7ae904
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2019-09-03 19:31:29 +0500

    Update Group.cs

--- Group.cs
+++ Group.cs
@@ -1,6 +1,6 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections.Generic;
-using ToolBox.Attributes;
+using NaughtyAttributes;
 
 [CreateAssetMenu(menuName = "ToolBox/Group")]
 public class Group : ScriptableObject
