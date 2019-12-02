b52addefcf71e751a0d5502c1db8422a51d478cd
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2019-12-02 21:04:40 +0500

    Relationships System

---
+++ Relationships.cs
@@ -0,0 +1,23 @@
+﻿using ToolBox.Attributes;
+using UnityEngine;
+
+namespace ToolBox.Groups.Utilities
+{
+	public class Relationships : MonoBehaviour
+	{
+		public Group[] Allies => GetGroup(allies);
+		public Group[] Enemies => GetGroup(enemies);
+		public Group[] Neutrals => GetGroup(neutrals);
+
+		[SerializeField, ReorderableList] private Group[] allies = null;
+		[SerializeField, ReorderableList] private Group[] enemies = null;
+		[SerializeField, ReorderableList] private Group[] neutrals = null;
+
+		private Group[] GetGroup(Group[] group)
+		{
+			Group[] newGroup = new Group[group.Length];
+			group.CopyTo(newGroup, 0);
+			return newGroup;
+		}
+	}
+}
