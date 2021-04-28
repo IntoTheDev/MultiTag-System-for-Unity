ae8375227d15abb7f8a2d3c0d040be53579f5590
Vladislav Kinyashov <indraayy322@gmail.com>
2021-04-28 20:01:25 +0500

    Update TagExtensions.cs

--- Runtime/TagExtensions.cs
+++ Runtime/TagExtensions.cs
@@ -5,31 +5,35 @@ namespace ToolBox.Tags
 	public static class TagExtensions
 	{
 		public static void AddTag(this GameObject instance, Tag tag) =>
-			tag.Add(instance);
+			tag.Add(instance, instance.GetHashCode());
 
 		public static void RemoveTag(this GameObject instance, Tag tag) =>
-			tag.Remove(instance);
+			tag.Remove(instance, instance.GetHashCode());
 
 		public static bool HasTag(this GameObject entity, Tag tag) =>
 			tag.HasInstance(entity.GetHashCode());
 
 		public static void AddTags(this GameObject instance, Tag[] tags)
 		{
+			int hash = instance.GetHashCode();
+
 			for (int i = 0; i < tags.Length; i++)
-				tags[i].Add(instance);
+				tags[i].Add(instance, hash);
 		}
 
 		public static void AddTags(this GameObject instance, TagsContainer container) =>
-			container.Add(instance);
+			container.Add(instance, instance.GetHashCode());
 
 		public static void RemoveTags(this GameObject instance, Tag[] tags)
 		{
+			int hash = instance.GetHashCode();
+
 			for (int i = 0; i < tags.Length; i++)
-				tags[i].Remove(instance);
+				tags[i].Remove(instance, hash);
 		}
 
 		public static void RemoveTags(this GameObject instance, TagsContainer container) =>
-			container.Remove(instance);
+			container.Remove(instance, instance.GetHashCode());
 
 		public static bool HasTags(this GameObject instance, Tag[] tags, bool allRequired)
 		{
